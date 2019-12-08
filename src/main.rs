987f55eea01b943202c19a184272a216234f334c
Tim Crawford <crawfxrd@gmail.com>
2019-12-08 14:38:02 -0700

    input: Change color when a key is pressed

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ type IrqHandler = unsafe extern "C" fn();
 const IRQ_HANDLER: Register<IrqHandler, ReadWrite> = Register::new(0x0300_7FFC);
 
 extern "C" {
-    pub fn master_isr();
+    fn master_isr();
 }
 
 const DISPCNT: Register<u16, ReadWrite> = Register::new(0x0400_0000);
@@ -30,6 +30,61 @@ const VCOUNT: Register<u16, ReadOnly> = Register::new(0x0400_0006);
 
 const IE: Register<u16, ReadWrite> = Register::new(0x0400_0200);
 const IME: Register<u16, ReadWrite> = Register::new(0x0400_0208);
+
+const KEYINPUT: Register<u16, ReadOnly> = Register::new(0x0400_0130);
+const KEY_A: u16 = 1 << 0;
+const KEY_B: u16 = 1 << 1;
+const KEY_SELECT: u16 = 1 << 2;
+const KEY_START: u16 = 1 << 3;
+const KEY_RIGHT: u16 = 1 << 4;
+const KEY_LEFT: u16 = 1 << 5;
+const KEY_UP: u16 = 1 << 6;
+const KEY_DOWN: u16 = 1 << 7;
+const KEY_R: u16 = 1 << 8;
+const KEY_L: u16 = 1 << 9;
+const KEY_MASK: u16 = 0b0000_0011_1111_1111;
+
+static mut INPUT_PREV: u16 = 0;
+static mut INPUT_CURRENT: u16 = 0;
+
+fn key_current_state() -> u16 {
+    unsafe { INPUT_CURRENT }
+}
+
+fn key_prev_state() -> u16 {
+    unsafe { INPUT_PREV }
+}
+
+fn key_poll() {
+    unsafe {
+        INPUT_PREV = INPUT_CURRENT;
+        INPUT_CURRENT = !KEYINPUT.read() & KEY_MASK;
+    }
+}
+
+fn key_is_down(key: u16) -> bool {
+    (key_current_state() & key) != 0
+}
+
+fn key_is_up(key: u16) -> bool {
+    (!key_current_state() & key) != 0
+}
+
+fn key_was_down(key: u16) -> bool {
+    (key_prev_state() & key) != 0
+}
+
+fn key_was_up(key: u16) -> bool {
+    (!key_prev_state() & key) != 0
+}
+
+fn key_down(key: u16) -> bool {
+    key_was_up(key) && key_is_down(key)
+}
+
+fn key_up(key: u16) -> bool {
+    key_was_down(key) && key_is_up(key)
+}
 
 #[derive(PartialEq)]
 struct Color(u16);
@@ -41,6 +96,8 @@ impl Color {
 }
 
 const BLACK: Color = Color::new(0, 0, 0);
+const GRAY: Color = Color::new(0x08, 0x08, 0x08);
+const LIGHT_GRAY: Color = Color::new(0x1C, 0x1C, 0x1C);
 const WHITE: Color = Color::new(0x1F, 0x1F, 0x1F);
 const RED: Color = Color::new(0x1F, 0, 0);
 const GREEN: Color = Color::new(0, 0x1F, 0);
@@ -87,6 +144,29 @@ pub extern "C" fn main() -> ! {
     let mut color = WHITE;
     loop {
         vsync();
+        key_poll();
+
+        if key_down(KEY_A) {
+            color = CYAN;
+        } else if key_down(KEY_B) {
+            color = YELLOW;
+        } else if key_down(KEY_START) {
+            color = WHITE;
+        } else if key_down(KEY_SELECT) {
+            color = BLACK;
+        } else if key_down(KEY_RIGHT) {
+            color = RED;
+        } else if key_down(KEY_LEFT) {
+            color = GREEN;
+        } else if key_down(KEY_UP) {
+            color = BLUE;
+        } else if key_down(KEY_DOWN) {
+            color = MAGENTA;
+        } else if key_down(KEY_R) {
+            color = LIGHT_GRAY;
+        } else if key_down(KEY_L) {
+            color = GRAY;
+        }
 
         draw_pixel(x, y, &color);
 
@@ -97,7 +177,6 @@ pub extern "C" fn main() -> ! {
         }
         if y >= DISPLAY_HEIGHT {
             y = 0;
-            color = if color == BLACK { WHITE } else { BLACK };
         }
     }
 }

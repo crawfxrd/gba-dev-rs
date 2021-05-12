6b0f15a20f4916ff9062d78e8b9fd2ded9690116
Tim Crawford <crawfxrd@gmail.com>
2021-05-11 19:32:29 -0600

    gba: Use linker file for all targets

--- crates/gba/build.rs
+++ crates/gba/build.rs
@@ -5,7 +5,7 @@ use std::env;
 
 fn main() {
     let cwd = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-arg-bins=-T{}/src/linker.ld", cwd);
+    println!("cargo:rustc-link-arg=-T{}/src/linker.ld", cwd);
     let profile = env::var("PROFILE").unwrap();
     println!("cargo:rustc-link-arg-bins=-Map=target/{}.map", profile);
 

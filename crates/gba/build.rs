c55ff955d99bdf36a90ca2d960304b8254c88271
Tim Crawford <crawfxrd@gmail.com>
2021-07-21 19:43:57 -0600

    Move linker script to binary project
    Transitive linker args were broken. Move linker script from lib back to
    bin.
    
    Ref: https://github.com/rust-lang/cargo/issues/9554

--- crates/gba/build.rs
+++ crates/gba/build.rs
@@ -1,14 +1,7 @@
 // SPDX-FileCopyrightText: NONE
 // SPDX-License-Identifier: CC0-1.0
 
-use std::env;
-
 fn main() {
-    let cwd = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-arg=-T{}/src/linker.ld", cwd);
-    let profile = env::var("PROFILE").unwrap();
-    println!("cargo:rustc-link-arg-bins=-Map=target/{}.map", profile);
-
     cc::Build::new()
         .compiler("arm-none-eabi-gcc")
         .no_default_flags(true)

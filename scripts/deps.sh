4ad52224927ed05810825bae39e6dfdb9badea71
Tim Crawford <crawfxrd@gmail.com>
2022-02-15 21:14:06 -0700

    scripts: Sort distros for system deps

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: NONE
 
+# Install dependencies for building the project.
+
 set -eE
 
 # Print bolded message to stdout
@@ -9,21 +11,26 @@ msg() {
   echo -e "\x1B[1m$*\x1B[0m"
 }
 
-source /etc/os-release
-
 msg ">> Installing system dependencies"
-if [[ "${ID}" = "fedora" ]] || [[ "${ID_LIKE}" =~ "fedora" ]]; then
+source /etc/os-release
+if [[ "${ID}" = "arch" ]] || [[ "${ID_LIKE}" =~ "arch" ]]; then
+    sudo pacman -S --noconfirm \
+        arm-none-eabi-binutils \
+        arm-none-eabi-gcc \
+        curl
+elif [[ "${ID}" = "fedora" ]] || [[ "${ID_LIKE}" =~ "fedora" ]]; then
     sudo dnf -y install \
         arm-none-eabi-gcc-cs \
-        arm-none-eabi-newlib
+        arm-none-eabi-newlib \
+        curl
 elif [[ "${ID}" = "ubuntu" ]] || [[ "${ID_LIKE}" =~ "debian" ]]; then
     sudo apt install --no-install-recommends --assume-yes \
         binutils-arm-none-eabi \
+        curl \
         gcc-arm-none-eabi
-elif [[ "${ID}" = "arch" ]] || [[ "${ID_LIKE}" =~ "arch" ]]; then
-    sudo pacman -S --noconfirm \
-        arm-none-eabi-binutils \
-        arm-none-eabi-gcc
+else
+    msg "\x1B[31m>> Unknown host:\x1B[0m ${ID}"
+    exit 1
 fi
 
 if which rustup &> /dev/null; then
@@ -39,4 +46,8 @@ else
 fi
 
 msg ">> Installing pinned Rust toolchain and components"
+# rustup has no command to specifically install a toolchain from a TOML file.
+# Rely on the fact that `show` will install the default toolchain.
 rustup show
+
+msg "\x1B[32m>> Successfully installed dependencies\n"

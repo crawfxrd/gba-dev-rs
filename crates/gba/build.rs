ab9b8765a39110ec38cb41fc1965058e27629b8b
Tim Crawford <crawfxrd@gmail.com>
2022-03-29 19:14:34 -0600

    Fix building mode3 example

--- crates/gba/build.rs
+++ crates/gba/build.rs
@@ -1,7 +1,12 @@
 // SPDX-FileCopyrightText: NONE
 // SPDX-License-Identifier: CC0-1.0
 
+use std::env;
+
 fn main() {
+    let cwd = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-link-arg-examples=-T{}/linker.ld", cwd);
+
     cc::Build::new()
         .compiler("arm-none-eabi-gcc")
         .no_default_flags(true)
--- crates/gba/examples/mode3/main.rs
+++ crates/gba/examples/mode3/main.rs
@@ -7,7 +7,7 @@
 #[rustfmt::skip]
 mod cyberpunk;
 
-use gba::{bios, interrupt};
+use gba::{bios, entry, interrupt};
 
 const DISPCNT: *mut u16 = 0x0400_0000 as *mut u16;
 const VRAM: *mut u16 = 0x0600_0000 as *mut u16;
@@ -15,8 +15,8 @@ const VRAM: *mut u16 = 0x0600_0000 as *mut u16;
 const MODE3: u16 = 0x3;
 const ENABLE_BG2: u16 = 1 << 10;
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+#[entry]
+fn main() {
     interrupt::init(interrupt::master_isr);
     interrupt::enable(interrupt::Irq::VBlank);
 
@@ -32,13 +32,5 @@ pub extern "C" fn main() -> ! {
 
     loop {
         bios::vblank();
-    }
-}
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    interrupt::reset();
-    loop {
-        bios::halt();
     }
 }

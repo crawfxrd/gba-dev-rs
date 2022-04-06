3efa99f4d6379e38f544ccdf5b9e2cbf0aaf2815
Tim Crawford <crawfxrd@gmail.com>
2022-04-06 08:04:14 -0600

    scripts: Move ROMs to dir for build type

--- scripts/roms.sh
+++ scripts/roms.sh
@@ -4,8 +4,13 @@
 
 set -e
 
+NAME="untitled"
+TARGET="thumbv4t-none-eabi"
+DEBUG_DIR="target/${TARGET}/debug"
+RELEASE_DIR="target/${TARGET}/release"
+
 cargo build
-arm-none-eabi-objcopy -O binary ./target/thumbv4t-none-eabi/debug/untitled ./target/debug.gba
+arm-none-eabi-objcopy -O binary ${DEBUG_DIR}/${NAME} ${DEBUG_DIR}/${NAME}.gba
 
 cargo build --release
-arm-none-eabi-objcopy -O binary ./target/thumbv4t-none-eabi/release/untitled ./target/release.gba
+arm-none-eabi-objcopy -O binary ${RELEASE_DIR}/${NAME} ${RELEASE_DIR}/${NAME}.gba

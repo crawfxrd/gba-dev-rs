9e6d6dde32c839440e91fb113265720531a3a168
Tim Crawford <crawfxrd@gmail.com>
2022-07-23 00:54:45 -0600

    scripts: Move formatting messages to function
    Remove the ">> " prefix from every message and do it once in the
    function. It's no longer colored, but that's not important.

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,10 +8,10 @@ set -eE
 
 # Print bolded message to stdout
 msg() {
-  echo -e "\x1B[1m$*\x1B[0m"
+  echo -e "\n\x1B[1m>> $*\x1B[0m"
 }
 
-msg ">> Installing system dependencies"
+msg "Installing system dependencies"
 source /etc/os-release
 if [[ "${ID}" = "arch" ]] || [[ "${ID_LIKE}" =~ "arch" ]]; then
     sudo pacman -S --noconfirm \
@@ -29,28 +29,28 @@ elif [[ "${ID}" = "ubuntu" ]] || [[ "${ID_LIKE}" =~ "debian" ]]; then
         curl \
         gcc-arm-none-eabi
 else
-    msg "\x1B[31m>> Unknown host:\x1B[0m ${ID}"
+    msg "\x1B[31mUnknown host:\x1B[0m ${ID}"
     exit 1
 fi
 
 if which rustup &> /dev/null; then
-    msg ">> Updating rustup"
+    msg "Updating rustup"
     rustup self update
 else
-    msg ">> Installing Rust"
+    msg "Installing Rust"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
         | sh -s -- -y --default-toolchain none
 
-    msg ">> Loading Rust environment"
+    msg "Loading Rust environment"
     source "${HOME}/.cargo/env"
 fi
 
-msg ">> Installing pinned Rust toolchain and components"
+msg "Installing pinned Rust toolchain and components"
 # rustup has no command to specifically install a toolchain from a TOML file.
 # Rely on the fact that `show` will install the default toolchain.
 rustup show
 
-msg ">> Installing git hooks"
+msg "Installing git hooks"
 ./scripts/git-hooks.sh
 
-msg "\x1B[32m>> Successfully installed dependencies\n"
+msg "\x1B[32mSuccessfully installed dependencies\n"
